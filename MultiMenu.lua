0a5e2baf689677f6a9566d1ab4e46de3eca071e5
369miko <farmikboosty@gmail.com>
2026-09-04 23:24:05 +0300

    Update MultiMenu.lua

--- MultiMenu.lua
+++ MultiMenu.lua
@@ -1,6 +1,6 @@
 script_name("MultiMenu")
 script_author("369Miko")
-script_version("1.80")
+script_version("1.81")
 script_description("Мульти-менюшка с нужными фишечками для гейзоновцев.")
 
 require "lib.moonloader"
